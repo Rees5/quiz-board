4458f89937cb9cab10be060df37e978d3b3717cd
Rees Alumasa <reesalumasa@gmail.com>
2019-02-22 12:45:06 +0300

    Add initial modifications to css, js and quiz files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#quiz").submit(function(event) {
+    event.preventDefault();
+    var q1 = parseInt($("input:radio[name=1]:checked").val());
+    var q2 = parseInt($("input:radio[name=2]:checked").val());
+    var q3 = parseInt($("input:radio[name=3]:checked").val());
+    var q4 = parseInt($("input:radio[name=4]:checked").val());
+    var q5 = parseInt($("input:radio[name=5]:checked").val());
+    var q6 = parseInt($("input:radio[name=6]:checked").val());
+    $("#display").text(q1+q2+q3+q4+q5+q6);
+  });
+});
